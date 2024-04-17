3895699b71b91f1e9f2bf9e8a4ab8f23fff03927
Pitoo <abzarium@gmail.com>
2024-04-17 18:37:10 +0200

    level0

--- hangman.c
+++ hangman.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 #include "hangman.h"
 
 
@@ -20,8 +21,10 @@ int get_word(char secret[]){
     do{
         // generate random number between 0 and filesize
         long int random = (rand() % size) + 1;
+
         // seek to the random position of file
         fseek(fp, random, SEEK_SET);
+
         // get next word in row ;)
         int result = fscanf(fp, "%*s %20s", secret);
         if( result != EOF )
@@ -31,4 +34,38 @@ int get_word(char secret[]){
     fclose(fp);
 
     return 0;
+}
+
+
+
+int is_word_guessed(const char secret[], const char letters_guessed[]) {
+    int findFlag = 0;
+    for (int i = 0; i < strlen(secret); i++) {
+        findFlag = 0;
+        for (int j = 0; j < strlen(letters_guessed); j++) {
+            if (secret[i] == letters_guessed[j]) {
+                findFlag = 1;
+                break;
+            }
+        }
+        if (findFlag == 0) {
+            return 0;
+        }   
+    }
+    return 1;
+}
+
+
+
+void get_guessed_word(const char secret[], const char letters_guessed[], char guessed_word[]) {
+    for (int i = 0; i < strlen(secret); i++) {
+        for (int j = 0; j < strlen(letters_guessed); j++) {
+            if (secret[i] == letters_guessed[j]) {
+                guessed_word[i]=secret[i];
+                break;
+            } else {
+                guessed_word[i]='_';
+            }
+        }
+    } 
 }
--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include "hangman.h"
+#include <stdlib.h>
+#include <time.h>
+
 int main(){
 
+    srand(time(NULL));
+
+    char secret[16];
+    char result[30];
+
+    get_word(secret);
+    printf("%s\n", secret);
     
+    printf("%d\n", is_word_guessed("secret", "aeiou"));
+    // prints: 0
+    printf("%d\n", is_word_guessed("hello", "aeihoul"));
+    // prints: 1
+
+    get_guessed_word("container", "arpstxgoieyu", result);
+    printf("%s\n", result);
+    // result = "_o_tai_er"
+
+
     return 0;
 }
